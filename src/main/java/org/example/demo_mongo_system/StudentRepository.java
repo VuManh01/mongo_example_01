8d21b302dc097f11abfb9fce89488b171c701f6a
VuManh01 <vutrongmanhmk@gmail.com>
2024-08-14 12:04:46 +0700

    version2

--- src/main/java/org/example/demo_mongo_system/StudentRepository.java
+++ src/main/java/org/example/demo_mongo_system/StudentRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface    StudentRepository extends MongoRepository<Student,String> {
-    @Override
     List<Student> findAll();
 }
